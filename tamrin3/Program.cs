2bf82870a8dde7667da70f9c94ee0792888d2a8d
Pouyan <Pouyan@DESKTOP-SV001C7>
2023-06-25 11:54:00 +0430

    Add project files.

---
+++ tamrin3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace test
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter x: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter y: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            Calc(x, y);
+        }
+        private static void Calc(int x, int y)
+        {
+            double Result = Math.Sqrt(Math.Abs(x) + Math.Pow(y, 2));
+            Console.WriteLine("Reasult: " + Result);
+
+        }
+
+    }
+}
